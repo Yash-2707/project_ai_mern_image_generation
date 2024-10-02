8679c113a8a4f5623594e18501b33faa0787884c
YASH <139299779+Yash-2707@users.noreply.github.com>
2024-10-02 14:14:39 +0530

    Update index.js
    IMPROVED CORS AND ADDED SECURITY FEATURE TO PROTECT FROM CROSS SITE SCRIPTING

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import * as dotenv from 'dotenv';
 import cors from 'cors';
+import helmet from 'helmet';
 
 import connectDB from './mongodb/connect.js';
 import postRoutes from './routes/postRoutes.js';
@@ -9,8 +10,11 @@ import dalleRoutes from './routes/dalleRoutes.js';
 dotenv.config();
 
 const app = express();
-app.use(cors());
-app.use(express.json({ limit: '50mb' }));
+app.use(cors({
+  origin: process.env.FRONTEND_URL,
+  methods: ['POST', 'GET', 'PUT', 'DELETE'],
+  credentials: true, // if you need cookies or authorization headers from the frontend
+}));app.use(express.json({ limit: '50mb' }));
 
 app.use('/api/v1/post', postRoutes);
 app.use('/api/v1/dalle', dalleRoutes);
@@ -20,6 +24,7 @@ app.get('/', async (req, res) => {
     message: 'Hello from DALL.E!',
   });
 });
+app.use(helmet());// TO PROTECT FROM CROSS SITE SCRIPTING
 
 const startServer = async () => {
   try {
